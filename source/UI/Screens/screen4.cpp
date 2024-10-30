9d4dd1d117e1012da4da5447e48652cd3f72e766
cs18472 <CWS18472@gmail.com>
2024-10-30 11:36:52 -0400

    enter changes to screen 4

--- source/UI/Screens/screen4.cpp
+++ source/UI/Screens/screen4.cpp
@@ -26,19 +26,17 @@ Screen_State screen_state = DEFAULT;
 
 void Screen4Enter(void)
 {
-
-}
-
-void Screen4Create(void)
-{
-	vLcdBlankerEx(WHITE, ALPHA_COLOR);
-
 	ScreensSetupDefaultKeys();
 	ButtonBarSetKeyImages(KEYINDEX_1, &view_meters, &view_meters);
 	ButtonBarSetKeyImages(KEYINDEX_2, &blank, &blank);
 	ButtonBarSetKeyImages(KEYINDEX_3, &thrust_calib, &thrust_calib);
 	ButtonBarSetKeyImages(KEYINDEX_4, &init_cal, &init_cal);
 	ButtonBarSetKeyImages(KEYINDEX_5, &blank, &blank);
+}
+
+void Screen4Create(void)
+{
+	vLcdBlankerEx(WHITE, ALPHA_COLOR);
 
 	ButtonBarSetMode(BUTTONBARMODE_VISIBLE_ALWAYS);
 
